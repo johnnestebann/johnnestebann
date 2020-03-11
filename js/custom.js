690b48a0d135ae0fd5e2a0e0eda4e66dae744b28
johnnestebann <38429880Jeaa>
2020-03-11 00:40:25 -0300

    update

--- js/custom.js
+++ js/custom.js
@@ -2,16 +2,13 @@
 
   "use strict";
 
-    // COLOR MODE
     $('.color-mode').click(function(){
         $('.color-mode-icon').toggleClass('active')
         $('body').toggleClass('dark-mode')
     })
 
-    // HEADER
     $(".navbar").headroom();
 
-    // PROJECT CAROUSEL
     $('.owl-carousel').owlCarousel({
     	items: 1,
 	    loop:true,
@@ -19,7 +16,6 @@
 	    nav:true
 	});
 
-    // SMOOTHSCROLL
     $(function() {
       $('.nav-link, .custom-btn-link').on('click', function(event) {
         var $anchor = $(this);
@@ -30,7 +26,6 @@
       });
     });  
 
-    // TOOLTIP
     $('.social-links a').tooltip();
 
 })(jQuery);
